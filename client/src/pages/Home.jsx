f01746d20ed74afae4f25b49b0477f2d66fa7e68
siobhan <sr@saibhreas.com>
2023-09-07 14:20:17 -0400

    cannot get GET /api/auth/signin HTTP/1.1 not sure why. fixed couple of small typose on home page but cannot find the source of thei porblem. rolling back the route and auth files to start over.

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function home() {
   return (
-    <div class="h-screen flex items-center justify-center">
+    <div className="h-screen flex items-center justify-center">
       <div>
         <h2 className='text-blue-500'>HOME</h2>
       </div>
