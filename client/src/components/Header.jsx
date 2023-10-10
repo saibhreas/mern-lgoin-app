bffb041bc022d6d6612677932f78880b4eb96276
siobhan <sr@saibhreas.com>
2023-10-09 21:52:23 -0400

    user icon added, now integrating private page

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,6 +1,8 @@
 import {Link} from 'react-router-dom'
+import { useSelector } from 'react-redux';
 
 export default function Header() {
+  const { currentUser } = useSelector((state) =>state.user);
   return (
     <div className='bg-slate-200'>
       <div className="flex justify-between items-center max-w-6xl max-auto p-3">
@@ -14,11 +16,16 @@ export default function Header() {
           <Link to='/about'>
             <li>About</li>
             </Link>
-          <Link to='sign-in'><li>Sign In</li></Link>
+          <Link to='sign-in'>
+            {currentUser ? (
+              <img src={currentUser.profilePicture} alt='icon'className='h-7 w-7 rounded-full object-cover'  />
+            ) : (
+              <li>Sign In</li>
+            )}
+          </Link>
           <Link to='sign-up'><li>Sign Up</li></Link>
         </ul>
-       
-      </div>
+     </div>
       
       
       
