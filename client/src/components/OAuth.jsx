c8e54039f0772f142595a23dd290dc86e11f3a5b
siobhan <sr@saibhreas.com>
2023-10-14 15:05:56 -0400

    app kept crashing saying json error at end of handleGoogleClick. Kept attributing it to Qauth, but tht was not changed.  reverted back to end of lesson 20, now restarting --completting image uplad functionality

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -11,9 +11,8 @@ export default function OAuth() {
     try {
       const provider = new GoogleAuthProvider();
       const auth = getAuth(app);
-      
+
       const result = await signInWithPopup(auth, provider);
-      
       const res = await fetch('/api/auth/google', {
         method: 'POST',
         headers: {
@@ -25,7 +24,6 @@ export default function OAuth() {
           photo: result.user.photoURL,
         }),
       });
-    
       const data = await res.json();
       console.log(data);
       dispatch(signInSuccess(data));
@@ -34,15 +32,13 @@ export default function OAuth() {
       console.log('could not login with google', error);
     }
   };
-
   return (
     <button
       type='button'
       onClick={handleGoogleClick}
-      className='bg-red-700 text-white uppercase rounded-lg p-3 hover:opacity-80'
+      className='bg-red-700 text-white rounded-lg p-3 uppercase hover:opacity-95'
     >
-      {' '}
-      Continue with Google
+      Continue with google
     </button>
   );
 }
