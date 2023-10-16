d4dda55b25891190684cdc470e54f46496739731
siobhan <sr@saibhreas.com>
2023-10-16 18:11:51 -0400

    found missing state declearation.  Good up to mid point of lesson UploadTask working. can bee seen in firebase.

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,30 +1,47 @@
 import { useSelector } from 'react-redux';
 import { useRef, useState, useEffect } from 'react';
+import { getStorage, ref, uploadBytesResumable } from 'firebase/storage';
+import { app } from '../firebase';
 
 export default function Profile() {
   const fileRef = useRef(null);
-  const[image, setImage] = useState(undefined);
+  const [image, setImage] = useState(undefined);
+  const [imagePercent, setImagePercent] = useState(0);
   console.log(image);
   const { currentUser } = useSelector((state) => state.user);
-  useEffect(() =>{
+  useEffect(() => {
     if (image) {
       handleFileUpload(image);
     }
-  },[image] );
-  const handleFileUpload = async (image)=> {
-    console.log(image);
-  }
+  }, [image]);
+  const handleFileUpload = async (image) => {
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + image.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, image);
+    uploadTask.on(
+      'state_changed',
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setImagePercent(Math.round(progress));
+        console.log(progress);
+      }
+ 
+    );
+  };
 
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className=' text-3xl text-center font-semibold my-7'>Profile</h1>
       <form className='flex flex-col gap-4'>
-        <input 
-          type='file' 
-          ref={fileRef} 
-          accept='image/*' hidden
-          onChange={(e) => setImage(e.target.files[0])} 
-          />
+        <input
+          type='file'
+          ref={fileRef}
+          accept='image/*'
+          hidden
+          onChange={(e) => setImage(e.target.files[0])}
+        />
         {/* 
       firebase storage rules:  
       allow read;
